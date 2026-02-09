e7214d3e2bea9c30f18c19db59cb4355f8ff6384
theun1c <belovdima1122@yandex.ru>
2026-02-09 19:43:42 +0300

    add project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleFurnitureApp.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
